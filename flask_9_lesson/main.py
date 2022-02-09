ae3c32c9e53093a7340e4141149e4e8061b38006
knQzx <75641500+knQzx@users.noreply.github.com>
2022-02-09 23:22:14 +0300

    Create main.py

---
+++ flask_9_lesson/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Домашняя страница')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
